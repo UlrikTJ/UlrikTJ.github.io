d2dd86911e33fdcc7d351bca7db430d9c02babed
UlrikTJ <ulrikj2003@gmail.com>
2023-12-27 18:39:48 +0100

    Added a localhost json server outputting the values

--- scripts.js
+++ scripts.js
@@ -1,49 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Function to fetch auction data from the server
-    function fetchAuctionData() {
-        // Make an AJAX request to your server endpoint
-        // Modify the URL to match the endpoint where your Python server serves auction data
-        fetch('https://your-server-endpoint.com/auction_data')
-            .then(response => response.json())
-            .then(data => {
-                // Process fetched data and display it on the webpage
-                displayAuctionData(data);
-            })
-            .catch(error => {
-                console.error('Error fetching auction data:', error);
-            });
-    }
+// Fetch the data from the local server
+fetch('http://localhost:8000/auction_results.json')
+    .then(response => response.json()) // Convert the response to a JavaScript object
+    .then(data => {
+        // Get the #auctions section
+        const auctionsSection = document.querySelector('#auctions');
 
-    // Function to display auction data on the webpage
-    function displayAuctionData(data) {
-        const auctionsSection = document.getElementById('auctions');
+        // Loop over the array of objects
+        data.forEach(item => {
+            // Create a new div element
+            const div = document.createElement('div');
 
-        // Clear previous content
-        auctionsSection.innerHTML = '';
+            // Set the text content of the div
+            div.textContent = `Name: ${item.name}, Lowest Bin: ${item.lowest_bin}, Second Lowest Bin: ${item.second_lowest_bin}, Profit: ${item.profit}, Profit Percentage: ${item.profit_percentage}, Items in AH: ${item.items_in_ah}, Item Category: ${item.item_category}`;
 
-        // Create HTML elements to display auction data
-        data.forEach(auction => {
-            const auctionElement = document.createElement('div');
-            auctionElement.classList.add('auction-item');
-
-            // Customize the content based on your data structure
-            auctionElement.innerHTML = `
-                <h2>${auction.name}</h2>
-                <p>Lowest Bin: ${auction.lowest_bin}</p>
-                <p>Second Lowest Bin: ${auction.second_lowest_bin}</p>
-                <p>Profit: ${auction.profit}</p>
-                <p>Profit Percentage: ${auction.profit_percentage}</p>
-                <p>Items in AH: ${auction.items_in_ah}</p>
-                <p>Item Category: ${auction.item_category}</p>
-            `;
-
-            auctionsSection.appendChild(auctionElement);
+            // Append the div to the #auctions section
+            auctionsSection.appendChild(div);
         });
-    }
-
-    // Fetch auction data initially when the page loads
-    fetchAuctionData();
-
-    // Set an interval to refresh the data periodically (adjust the interval as needed)
-    setInterval(fetchAuctionData, 30000); // Refresh every 30 seconds
-});
+    })
+    .catch(error => console.error('Error:', error));
