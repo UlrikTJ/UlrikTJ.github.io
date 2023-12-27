4e651626a4e381c58cf8a30e34e0e8d1358f5a7c
UlrikTJ <ulrikj2003@gmail.com>
2023-12-27 18:58:38 +0100

    Making the output show on the website

--- scripts.js
+++ scripts.js
@@ -10,8 +10,19 @@ fetch('http://localhost:8000/auction_results.json')
             // Create a new div element
             const div = document.createElement('div');
 
+            // Add a class to the div
+            div.classList.add('item-box');
+
             // Set the text content of the div
-            div.textContent = `Name: ${item.name}, Lowest Bin: ${item.lowest_bin}, Second Lowest Bin: ${item.second_lowest_bin}, Profit: ${item.profit}, Profit Percentage: ${item.profit_percentage}, Items in AH: ${item.items_in_ah}, Item Category: ${item.item_category}`;
+            div.innerHTML = `
+                <h2>${item.name}</h2>
+                <p>Lowest Bin: ${item.lowest_bin}</p>
+                <p>Second Lowest Bin: ${item.second_lowest_bin}</p>
+                <p>Profit: ${item.profit}</p>
+                <p>Profit Percentage: ${item.profit_percentage}</p>
+                <p>Items in AH: ${item.items_in_ah}</p>
+                <p>Item Category: ${item.item_category}</p>
+            `;
 
             // Append the div to the #auctions section
             auctionsSection.appendChild(div);
