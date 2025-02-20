d87e48667091c65f7a168a80ca1c4b60d630f191
UlrikTJ <ulrikj2003@gmail.com>
2025-02-20 21:11:50 +0000

    added nvme temps

--- system_info.py
+++ system_info.py
@@ -2,16 +2,6 @@ import psutil
 import json
 import subprocess
 
-def get_system_info():
-    info = {
-        "cpu_usage": psutil.cpu_percent(interval=1),
-        "memory_usage": psutil.virtual_memory().percent,
-        "disk_usage": psutil.disk_usage('/').percent,
-        "cpu_temp": get_cpu_temp(),
-        "gpu_temp": get_gpu_temp(),
-        "uptime": psutil.boot_time()
-    }
-    return info
 
 def get_cpu_temp():
     try:
@@ -32,6 +22,26 @@ def get_gpu_temp():
         return None
     except:
         return None
+def get_nvme_temp():
+    try:
+        output = subprocess.check_output(["sensors"], text=True)
+        for line in output.splitlines():
+            if "Composite" in line:
+                return float(line.split()[1].replace("°C", ""))
+        return None
+    except:
+        return None
 
+def get_system_info():
+    info = {
+        "cpu_usage": psutil.cpu_percent(interval=1),
+        "memory_usage": psutil.virtual_memory().percent,
+        "disk_usage": psutil.disk_usage('/').percent,
+        "cpu_temp": get_cpu_temp(),
+        "gpu_temp": get_gpu_temp(),
+        "nvme_temp": get_nvme_temp(),
+        "uptime": psutil.boot_time()
+    }
+    return info
 if __name__ == "__main__":
     print(json.dumps(get_system_info()))
