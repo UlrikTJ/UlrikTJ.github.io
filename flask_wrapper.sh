c58addc264ab16fcefaf6e5e55673f0cd1125200
UlrikTJ <ulrikj2003@gmail.com>
2025-03-30 20:24:05 +0000

    changed active days

--- flask_wrapper.sh
+++ flask_wrapper.sh
@@ -4,7 +4,7 @@
 HOUR=$(TZ="Europe/Copenhagen" date +%H)
 
 # Check if current time is within active hours (8-22)
-if [ "$HOUR" -ge 8 ] && [ "$HOUR" -lt 22 ]; then
+if [ "$HOUR" -ge 0 ] && [ "$HOUR" -lt 23.95 ]; then
     echo "Starting Flask server (active hours: 8am-10pm Copenhagen time)"
     exec /home/mainserver/myflask_venv/bin/python /home/mainserver/UlrikTJ.github.io/server.py
 else
